7ed3701903ed64f48ed05947cb4df6cd222c810a
Nils Martel <nilsmartel@yahoo.de>
2021-08-05 09:25:19 +0200

    Include help text for binary

--- pc.go
+++ pc.go
@@ -9,9 +9,9 @@ import (
 
 func main() {
 	args := os.Args
-	if len(args) < 2 {
-		// maybe print help here?
-		os.Exit(1)
+	if len(args) < 2 || args[1] == "--help" || args[1] == "-h" {
+		printHelp()
+		os.Exit(0)
 	}
 
 	for _, arg := range args[1:] {
@@ -32,9 +32,20 @@ func main() {
 
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(0)
+		os.Exit(1)
 	}
 
+}
+
+func printHelp() {
+	fmt.Println("pc\n" +
+		"	usage: 		pc <command> <...arguments>\n" +
+		"	example:	pc mv lecture_01.pdf '01 Lecture.pdf'\n" +
+		"\n" +
+		"	Using pc you can print commands, before executing them.\n" +
+		"	The arguments to pc are simply shell commands, along with their arguments.\n" +
+		"\n" +
+		"	This way you can easier document, what is happening behind the scenes.")
 }
 
 func escape(s string) string {
